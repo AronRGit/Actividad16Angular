4169d42c56bd887f339d5f2603dfd21c6272507c
AronRP <aron16d@gmail.com>
2023-01-22 12:00:02 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductosComponent } from './components/productos/productos.component';
 
-const routes: Routes = [];
+const routes: Routes =[
+  {
+    path:'',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path:'productos',
+    component: ProductosComponent 
+  },
+  {
+    path:'contacto',
+    component: ContactoComponent 
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
